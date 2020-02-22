5883c3e223265bd7eb783bd18a648488187cd85a
Chaz Housh <houshce29@outlook.com>
2020-02-21 18:27:32 -0600

    Initial commit

---
+++ src/main/java/dev/houshce29/cc/App.java
@@ -0,0 +1,8 @@
+package dev.houshce29.cc;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("DEV");
+    }
+}
---
+++ src/test/java/dev/houshce29/cc/AppTest.java
@@ -0,0 +1,11 @@
+package dev.houshce29.cc;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void testAppHasAGreeting() {
+        Assert.fail("This project has no QA!");
+    }
+}
